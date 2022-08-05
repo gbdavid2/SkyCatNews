c0da6f0daa12b63198d5ad9a74cf7020d06d31cc
gbdavid2 <gbdavid2@users.noreply.github.com>
2022-08-05 18:54:02 +0100

    Initial Commit

---
+++ SkyCatNews/SkyCatNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCatNewsApp.swift
+//  SkyCatNews
+//
+//  Created by David Garces on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SkyCatNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
