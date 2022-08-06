25c1ba3743444aad35f07fe33dca4e3625a89523
gbdavid2 <gbdavid2@users.noreply.github.com>
2022-08-06 10:31:20 +0100

    added model folder

---
+++ SkyCatNews/Model/Extensions/StringExtensions.swift
@@ -0,0 +1,34 @@
+//
+//  StringExtensions.swift
+//  SkyCatNews
+//
+//  Created by David Garces on 06/08/2022.
+//
+
+import SwiftUI
+
+extension String {
+    
+    // MARK: UI Strings
+    static var skyTitle = "Sky Cat News"
+    
+}
+
+extension Double {
+    // MARK: UI Font
+    static var titleSmall = 22.0
+    static var titleLarge = 34.0
+    
+    // MARK: UI Opacity
+    static var titleFaded = 0.7
+    static var titleStrong = 1.0
+}
+
+extension CGFloat {
+    // MARK: UI General padding
+    static var generalHorizontal = CGFloat(20)
+    
+    // MARK: UI Padding
+    static var titleHorizontal = CGFloat(20)
+    static var titleTop = CGFloat(24)
+}
